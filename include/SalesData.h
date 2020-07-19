d7c4404940fd445d537907bb8aa113ebc5f2b2b7
superkk72 <superkk72@outlook.com>
2020-07-19 17:19:09 +0800

    code for c++ primer chapter7 sales_data class

---
+++ include/SalesData.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Sales_data {
+//友元声明
+friend Sales_data add(const Sales_data&, const Sales_data&);
+friend std::istream& read(std::istream&, Sales_data&);
+friend std::ostream& print(std::ostream&, const Sales_data&);
+
+public:
+	//构造函数
+	Sales_data() = default;
+	Sales_data(const std::string &s, unsigned n, double p):
+				bookNo(s), uints_sold(n), revenue(p*n) {}
+	Sales_data(const std::string &s): bookNo(s) {}
+	Sales_data(std::istream&);
+	//返回书籍的isbn号
+	std::string isbn() const { return bookNo; }
+	//执行Sales_data的+=操作，related to be replaced by += operator
+	Sales_data& combine(const Sales_data&);
+private:
+	double avg_price() const { return uints_sold ? revenue / uints_sold : 0; }
+	std::string bookNo;//书籍的isbn号
+	unsigned uints_sold = 0;//书籍的售出数目
+	double revenue = 0.;//书籍的销售额
+
+};
+
+Sales_data add(const Sales_data&, const Sales_data&);
+
+std::istream& read(std::istream&, Sales_data&);
+
+std::ostream& print(std::ostream&, const Sales_data&);
+
+
+
+
+
+
+
---
+++ src/SalesData.cpp
@@ -0,0 +1,46 @@
+﻿// SalesData.cpp : 定义静态库的函数。
+//
+#include "framework.h"
+#include "SalesData.h"
+#include <cassert>
+//#include <iostream>
+
+Sales_data::Sales_data(std::istream& is) {
+	read(is, *this);
+}
+
+Sales_data& Sales_data::combine(const Sales_data& item) {
+	assert(bookNo == item.bookNo);
+	uints_sold += item.uints_sold;
+	revenue += item.revenue;
+	return *this;
+}
+
+Sales_data add(const Sales_data& item1, const Sales_data& item2) {
+	assert(item1.bookNo == item2.bookNo);
+	Sales_data item;
+	item.bookNo = item1.bookNo;
+	item.uints_sold = item1.uints_sold + item2.uints_sold;
+	item.revenue = item1.revenue + item2.revenue;
+	return item;
+	
+}
+
+std::istream& read(std::istream& is, Sales_data& item) {
+	double price = 0.;
+	is >> item.bookNo >> item.uints_sold >> price;
+	item.revenue = item.uints_sold * price;
+	return is;
+}
+
+std::ostream& print(std::ostream& os, const Sales_data& item) {
+	os << item.bookNo << '\t' << item.uints_sold << '\t' <<
+		item.revenue << '\t' << item.avg_price();
+	return os;
+}
+
+
+
+
+// TODO: 这是一个库函数示例
+
