b83bde311e648ed75014b65258a8a859d797206e
superkk72 <superkk72@outlook.com>
2020-07-19 21:13:44 +0800

    add support of some operators

--- include/SalesData.h
+++ include/SalesData.h
@@ -7,6 +7,9 @@ class Sales_data {
 friend Sales_data add(const Sales_data&, const Sales_data&);
 friend std::istream& read(std::istream&, Sales_data&);
 friend std::ostream& print(std::ostream&, const Sales_data&);
+friend std::ostream& operator<<(std::ostream&, const Sales_data&);
+friend std::istream& operator>>(std::istream&, Sales_data&);
+friend Sales_data operator+(const Sales_data&, const Sales_data&);
 
 public:
 	//构造函数
@@ -19,6 +22,8 @@ public:
 	std::string isbn() const { return bookNo; }
 	//执行Sales_data的+=操作，related to be replaced by += operator
 	Sales_data& combine(const Sales_data&);
+	Sales_data& operator+=(const Sales_data&);
+
 private:
 	double avg_price() const { return uints_sold ? revenue / uints_sold : 0; }
 	std::string bookNo;//书籍的isbn号
@@ -32,6 +37,12 @@ Sales_data add(const Sales_data&, const Sales_data&);
 std::istream& read(std::istream&, Sales_data&);
 
 std::ostream& print(std::ostream&, const Sales_data&);
+//输入输出运算符必须为非成员函数
+std::ostream& operator<<(std::ostream&, const Sales_data&);
+
+std::istream& operator>>(std::istream&, Sales_data&);
+
+Sales_data operator+(const Sales_data&, const Sales_data&);
 
 
 
--- src/SalesData.cpp
+++ src/SalesData.cpp
@@ -39,6 +39,38 @@ std::ostream& print(std::ostream& os, const Sales_data& item) {
 	return os;
 }
 
+std::ostream& operator<<(std::ostream& os, const Sales_data& item) {
+	os << item.isbn() << " " << item.uints_sold << " " << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Sales_data& item) {
+	double price = 0.;
+	is >> item.bookNo >> item.uints_sold >> price;
+	if (is) {
+		item.revenue = item.uints_sold * price;
+	}
+	else {
+		item = Sales_data();
+	}
+	return is;
+
+}
+
+Sales_data& Sales_data::operator+=(const Sales_data& item) {
+	assert(bookNo == item.bookNo);
+	uints_sold += item.uints_sold;
+	revenue += item.revenue;
+	return *this;
+}
+
+Sales_data operator+(const Sales_data& item1, const Sales_data& item2) {
+	Sales_data sum = item1;
+	sum += item2;
+	return sum;
+}
+
+
 
 
 
